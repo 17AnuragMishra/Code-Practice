9689f4700c3abbe7b54312fc3790e9e635b224bb
Anurag Mishra <115710545+17AnuragMishra@users.noreply.github.com>
2023-11-17 20:58:30 +0530

    Add files via upload

---
+++ Pattern Printing/pattern.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n, m;
+    printf("Enter the rows: ");
+    scanf("%d", &n);
+    printf("Enter the column: ");
+    scanf("%d", &m);
+    for( int i=1; i<=n;i++){
+        for( int i=1; i<=m; i++)
+        printf("*");
+        printf("\n");
+    }
+    return 0;
+}
